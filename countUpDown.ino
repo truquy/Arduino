6ae8d1852cf713b7ec5b555da017a5e12dbac929
truquy <89262192+truquy@users.noreply.github.com>
2023-10-26 20:19:31 -0400

    Add files via upload

---
+++ countUpDown.ino
@@ -0,0 +1,26 @@
+int maxUp = 10;
+int minDown = 1;
+int delayT = 1000;
+int start = 0;
+int direction = 1;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  //start = maxUp % minDown;
+}
+
+void loop() {
+  //start = start + direction;
+
+  if(start>10 && start<1) {
+    direction = -1;
+    Serial.println(start);
+    delay(1000);
+  }
+  start = start + direction;
+  
+  
+
+  
+}
